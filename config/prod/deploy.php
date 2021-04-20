a000133d3a1649cb8483207aa9479348deb09bb8
primawebsystems <kostiaGm@gmail.com>
2021-04-20 23:26:18 +0300

    dfdfdf

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -2,13 +2,12 @@
 
 use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
 
-return new class extends DefaultDeployer
-{
-   // private $remoteComposerBinaryPath = '/usr/bin/composer';
+return new class extends DefaultDeployer {
+    // private $remoteComposerBinaryPath = '/usr/bin/composer';
     public function configure()
     {
         return $this->getConfigBuilder()
-            ->remoteComposerBinaryPath( '/usr/bin/composer')
+            ->remoteComposerBinaryPath('/usr/bin/composer')
             // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
             ->server(' kostiagm@92.205.24.68')
             // the absolute path of the remote server directory where the project is deployed
@@ -16,19 +15,20 @@ return new class extends DefaultDeployer
             // the URL of the Git repository where the project code is hosted
             ->repositoryUrl('git@github.com:martenasoft/study_organize_symfony.git')
             // the repository branch to deploy
-            ->repositoryBranch('master')
-        ;
+            ->repositoryBranch('master');
     }
 
-    // run some local or remote commands before the deployment is started
-    public function beforeStartingDeploy()
+    public function beforePreparing()
     {
-        // $this->runLocal('./vendor/bin/simple-phpunit');
+        $this->runRemote('cp {{ deploy_dir }}/repo/.env {{ project_dir }}/.env');
+     //   $this->runRemote('cp {{ deploy_dir }}/repo/.env.local {{ project_dir }}/.env.local');
     }
 
     // run some local or remote commands after the deployment is finished
     public function beforeFinishingDeploy()
     {
+        $this->runRemote('composer install');
+        $this->runRemote('{{ project_dir }}/bin/console doctrine:migrations:migrate --no-interaction');
         // $this->runRemote('{{ console_bin }} app:my-task-name');
         // $this->runLocal('say "The deployment has finished."');
     }
